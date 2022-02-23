26ef369e140a937e85d60fa5104f6959a71eba5f
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2022-02-23 21:55:50 +0200

    Control

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './sass/main.scss';
+import axios from 'axios';
 
-console.log('opopopopop');
+const BASE_URL = 'https://pixabay.com/api';
+const API_key = '25848819-9cdff21383665c4a2b048e17c';
+const formRef = document.querySelector('.search-form');
+const inputRef = document.querySelector('[name="searchQuery"]');
+const btnSubmitRef = document.querySelector('[type="submit"]');
+
+
+// fetch(`${BASE_URL}/?key=${API_key}&q=cat&image_type=photo`)
+//     .then(res => res.json())
+//     .then(console.log);
+
